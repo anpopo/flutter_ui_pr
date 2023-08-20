b60213af28b032ad57b57129b56bde54a161499c
ansehyeong <dkstpgud@gmail.com>
2023-08-20 23:54:10 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# flutter_ui_pr
+
+Flutter User Interface screen building practice.
+
+## User Interface Practice List
---
+++ android/app/src/main/kotlin/com/example/flutter_ui_pr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_ui_pr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
